bf812ca9de9df64108b36dfc0a834c5a84f827af
roulaoregan <roula.oregan@gmail.com>
2015-12-05 19:25:44 -0800

    Update triangulate.m
    updated version

--- triangulation/triangulate.m
+++ triangulation/triangulate.m
@@ -1,3 +1,4 @@
+
 function X = triangulate(xL,xR,camL,camR)
 
 %
@@ -13,77 +14,67 @@ function X = triangulate(xL,xR,camL,camR)
 %
 %    X : 3D coordinate of points in world coordinates (3xN array)
 %
-% 1. convert from pixel coordinates back into meters with unit focal 
-% length.  call the results xRm and xLm
 %
-% 2. make the left camera the origin of the world coordinate system by 
-% transforming both cameras appropriately in order to find the rotation
-% and translation (R,t) relating the two cameras
+
+if (size(xL,2) ~= size(xR,2))
+  error('must have same number of points in left and right images');
+end
+
+npts = size(xL,2);
+
+% first convert from pixel coordinates back into 
+% meters with unit focal length
+xLm(1,:) = (xL(1,:) - camL.c(1)) / (camL.f * camL.m(1));
+xLm(2,:) = (xL(2,:) - camL.c(2)) / (camL.f * camL.m(2));
+xLm(3,:) = 1;
+
+xRm(1,:) = (xR(1,:) - camR.c(1)) / (camR.f * camR.m(1));
+xRm(2,:) = (xR(2,:) - camR.c(2)) / (camR.f * camR.m(2));
+xRm(3,:) = 1;
+
 %
-% 3. solve the equation:  
-%   Z_r * xRm = R* Z_l * xLm + t
-% for the Zs using svd.m
+% make the left camera the origin of the world
+% coordinate system by transforming both cameras
+% appropriately
+%
+R = inv(camL.R);
+t = -camL.t;
+camRL.R = R*camR.R;
+camRL.t = R*(camR.t + t);
+camLL.R = R*camL.R;
+camLL.t = R*(camL.t + t);
 
-% 4. use Z_l compute the 3D coordinates XL in left camera reference frame
+%
+% for now assume that camL is at the origin.
+% express xLm in the right camera coordinate system
+%
+xLmR = inv(camRL.R)*xLm;
+tR = -inv(camRL.R)*repmat(camRL.t,1,npts);
 
-% 5. map XL back to world coordinates X
-
-[m,n] = size(xL);
-
-% STEP 1. convert from pixel coordinates back into meters with unit focal 
-% length.  call the results xRm and xLm
-xLm = ones(m,n);
-for i=1:m
-    for j=1:n
-        xtemp = (xL(1,j)- camL.c(1))/(camL.m(1)*camL.f);
-        ytemp = (xL(2,j) - camL.c(2))/(camL.m(2)*camL.f);
-        xLm(1,j) = xtemp;
-        xLm(2,j) = ytemp;        
-    end
+% now solve the equation:
+% 
+%   Z_r * xRm = Z_l * xLmR + tR
+%
+% for the Zs
+%
+Z_r = zeros(1,npts);
+Z_l = zeros(1,npts);
+for i = 1:npts
+  A = [xRm(:,i) -xLmR(:,i)];
+  Z = inv(A'*A)*A'*tR(:,i);
+  Z_r(i) = Z(1);
+  Z_l(i) = Z(2);
+  fprintf('\r %d/%d',i,npts);
 end
 
-xRm = ones(m,n);
-for i=1:m
-    for j=1:n
-        xtemp = (xR(1,j) - camR.c(1))/(camR.m(1)*camR.f);
-        ytemp = (xR(2,j)- camR.c(2))/(camR.m(2)*camR.f) ;
-        xRm(1,j) = xtemp;
-        xRm(2,j) = ytemp;
-    end
-end
-% 2. make the left camera the origin of the world coordinate system by 
-% transforming both cameras appropriately in order to find the rotation
-% and translation (R,t) relating the two cameras
+%compute coordinates in left camera ref frame
+XL = [Z_l;Z_l;Z_l].*[xLm(1:2,:);ones(1,npts)];
+XR = [Z_r;Z_r;Z_r].*[xRm(1:2,:);ones(1,npts)];
 
-%keep original camera positions for later
-originalCamL.R = camL.R;
-originalCamL.t = camL.t;
-originalCamR.R = camR.R;
-originalCamR.t = camR.t;
-%camL is at origin
-newcamL.R = camL.R'; %inverse rot
-newcamL.t = -camL.t; %translate
-%rotate camR in resp. to camL
-newcamR.R = camR.R*camL.R';
-newcamR.t = camL.R'*(camR.t-camL.t);
-%camRnew.t = inv(camL.R)*(camR.t-camL.t)
-%R = camR.R';
-%t = newcamR.t;%camR.t;
-hg = ones(1,n);
-xLm = [xLm;hg];
-xRm = [xRm;hg];
-xLmR = -newcamR.R'*xLm;
-% 3. solve the equation: 
-% Z_r * xRm = R* Z_l * xLm + t using least squares
-XL = ones(3,n);
-for i=1:n
-    A = [xRm(:,i) -xLmR(:,i)];
-    z = A\newcamR.t;   
-    XL(:,i) = z(2) * xLm(:,i);
-end
-% 4. use Z_l compute the 3D coordinates XL in left camera reference frame
-XL_hg = vertcat(XL, hg);
-%undo the rotation and translation
-rotateback_m = horzcat(originalCamL.R, originalCamL.t);
-% 5. map XL back to world coordinates X
-X = rotateback_m*XL_hg;
+%finally map X back to world coordinates 
+Xa = camL.R*XL + repmat(camL.t,1,npts);
+Xb = camR.R*XR + repmat(camR.t,1,npts);
+
+X = 0.5*(Xa+Xb);
+
+
